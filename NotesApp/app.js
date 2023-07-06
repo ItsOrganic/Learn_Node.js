9228ffb4de667e72a43f12d34b1417fe14899f9e
unknown <stutyashivam@gmail.com>
2023-07-06 21:44:52 +0530

    Notes_App

---
+++ NotesApp/app.js
@@ -0,0 +1,10 @@
+// const t  = require ('./utils.js')
+// const sum = t(4,5);
+// console.log(sum);
+
+const chalk = require('chalk')
+const getNotes = require('./notes.js');
+const yargs = require('yargs');
+
+console.log(process.argv)
+console.log(yargs.argv)
