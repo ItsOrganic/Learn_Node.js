9489d0c5ca1122e79a218e6950d0d46e0a1dbdaa
unknown <stutyashivam@gmail.com>
2023-07-09 11:58:59 +0530

    third

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -3,8 +3,28 @@
 // console.log(sum);
 
 const chalk = require('chalk')
-const getNotes = require('./notes.js');
 const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-console.log(process.argv)
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of note',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler:function(argv) {
+            console.log('Title = ' + argv.title)
+            console.log('Body = ' + argv.body)
+        }       
+})
 console.log(yargs.argv)
